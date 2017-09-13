6f10e32494f2b44baeb2074fdfb53ebf0320e16e
Pedro Abel <angeluz-abel@hotmail.es>
2017-09-13 13:13:50 -0500

    rename files

--- navarra/copyFiles/Program.cs
+++ navarra/copyFiles/Program.cs
@@ -7,6 +7,22 @@ namespace copyFiles
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        void RenameFiles()
+        {
+            string rutaPrincipal = @"C:\ACL\spm12\toolbox\ACL\Study\forg2";
+            foreach (var file in Directory.GetFiles(rutaPrincipal, "*.nii", SearchOption.AllDirectories))
+            {
+                FileInfo fileinf = new FileInfo(Path.GetFullPath(file));
+                string dir = Path.GetDirectoryName(file);
+                fileinf.MoveTo(dir + "\\" + "s" + Path.GetFileName(file));
+            }
+        }
+
+        void selectFiles()
+        {
             string rutaPrincipal = @"C:\ACL\spm12\toolbox\ACL\Study\StuPaci";
             string rutabackup = @"C:\Users\parivera\OneDrive\ESPOL\Bioingenieria\Studios Navarra\";
 
@@ -19,7 +35,7 @@ namespace copyFiles
             for (int i = 0; i < collec.Length; i++)
             {
                 foreach (var file in collec[i]) //extrae todos las coincidencias
-                {                    
+                {
                     var sujetoDir = rutabackup + Directory.GetParent(Path.Combine(Path.GetDirectoryName(file), @"..\..\")).Name;
                     if (!Directory.Exists(sujetoDir))
                     {
@@ -31,10 +47,10 @@ namespace copyFiles
                     //si se requiere poner en la carpeta padre se debe usar la siguiente funcion
                     //var chess = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), @"..\..\"));
                     //File.Copy(file, Path.Combine(chess, Path.GetFileName(file)), true);
-                    
+
                 }
             }
-            
+
             Console.Read();
         }
     }
